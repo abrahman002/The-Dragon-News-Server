6ad30f84ee378d28b680becb531935b5ac221f79
abrahman002 <rahmanx661@gmail.com>
2023-04-30 20:23:52 +0600

    vercel json file add

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port =process.env.port || 5000;
 const cors=require ('cors')
-const categories=require ('./data/categories.json')
+const categories=require ('./data/categories.json');
+const news=require('./data/news.json');
 
 app.use(cors());
 
@@ -13,6 +14,28 @@ app.get('/',(req,res)=>{
 
 app.get('/categories',(req,res)=>{
     res.send(categories);
+})
+
+app.get('/news',(req,res)=>{
+    res.send(news);
+})
+
+app.get('/news/:id',(req,res)=>{
+    const id=req.params.id;
+    const selectedNews=news.find(n => n._id === id);
+    res.send(selectedNews)
+});
+
+app.get('/categories/:id',(req,res)=>{
+    const id=parseInt(req.params.id);
+    
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+        const selectedCategory=news.filter(n => parseInt(n.category_id) === id );
+        res.send(selectedCategory);
+    }
 })
 app.listen(port,()=>{
     console.log(`Dragon news live on:${port}`)
